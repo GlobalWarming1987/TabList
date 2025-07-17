634ff89ef2b172da0d1d0445ec87673a6a3a435c
GlobalWarming1987 <juggalo19076@gmail.com>
2025-07-16 23:25:48 -0400

    Update IFakePlayer.java

--- bukkit/src/main/java/hu/montlikadani/tablist/tablist/fakeplayers/IFakePlayer.java
+++ bukkit/src/main/java/hu/montlikadani/tablist/tablist/fakeplayers/IFakePlayer.java
@@ -2,29 +2,27 @@ package hu.montlikadani.tablist.tablist.fakeplayers;
 
 import hu.montlikadani.tablist.tablist.player.PlayerSkinProperties;
 
-import java.util.UUID;
-
 public interface IFakePlayer {
 
 	String getName();
 
-	UUID getUniqueId();
-
-	void setDisplayName(String displayName);
+	void setName(String name);
 
 	String getDisplayName();
 
+	void setDisplayName(String displayName);
+
+	String getHeadId();
+
+	void setHeadId(String headId);
+
+	int getPingLatency();
+
 	void setPing(int ping);
-
-	int getPing();
-
-	String getHeadIdentifier();
-
-	void setSkin(PlayerSkinProperties props);
-
-	PlayerSkinProperties getSkinProperties();
 
 	void spawn();
 
 	void remove();
+
+	PlayerSkinProperties getSkinProperties();
 }
