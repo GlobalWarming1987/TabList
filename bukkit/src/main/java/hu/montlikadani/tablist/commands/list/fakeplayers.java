14355fdbd9b1a45602be75d2cec8ef109d21520c
GlobalWarming1987 <juggalo19076@gmail.com>
2025-07-16 22:55:31 -0400

    Update fakeplayers.java

--- bukkit/src/main/java/hu/montlikadani/tablist/commands/list/fakeplayers.java
+++ bukkit/src/main/java/hu/montlikadani/tablist/commands/list/fakeplayers.java
@@ -4,146 +4,145 @@ import hu.montlikadani.tablist.TabList;
 import hu.montlikadani.tablist.commands.Commands;
 import hu.montlikadani.tablist.tablist.fakeplayers.FakePlayerHandler;
 import hu.montlikadani.tablist.tablist.fakeplayers.FakePlayerHandler.EditingResult;
-import hu.montlikadani.tablist.tablist.player.TabListPlayer.PlayerSkinProperties;
+import hu.montlikadani.tablist.tablist.player.PlayerSkinProperties;
+import hu.montlikadani.tablist.tablist.fakeplayers.IFakePlayer;
 import hu.montlikadani.tablist.utils.Util;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.Set;
 
 public final class fakeplayers implements Commands.Command {
 
 	@Override
-	public void execute(TabList plugin, CommandSender sender, String[] args) {
+	public void perform(CommandSender sender, String[] args) {
 		if (args.length < 2) {
 			sendUsage(sender);
 			return;
 		}
 
-		FakePlayerHandler handler = plugin.getFakePlayerHandler();
+		FakePlayerHandler handler = TabList.getInstance().getFakePlayerHandler();
 		EditingResult output;
 
-		switch (args[1].toLowerCase()) {
-			case "add":
+		String sub = args[1].toLowerCase();
+
+		switch (sub) {
+			case "create":
 				if (args.length < 3) {
-					sender.sendMessage("§cUsage: /tablist fakeplayers add <name>");
+					sendUsage(sender);
 					return;
 				}
 
 				String name = args[2];
-
 				if ((output = handler.createPlayer(name, name, "", -1)) == EditingResult.ALREADY_EXIST) {
-					sender.sendMessage("§cFake player already exists.");
+					sender.sendMessage("Fake player already exists.");
 					return;
 				}
 
 				if (output == EditingResult.OK) {
-					sender.sendMessage("§aFake player added successfully.");
+					sender.sendMessage("Fake player created.");
 				}
-
 				break;
 
 			case "remove":
 				if (args.length < 3) {
-					sender.sendMessage("§cUsage: /tablist fakeplayers remove <name>");
+					sendUsage(sender);
 					return;
 				}
 
 				if ((output = handler.removePlayer(args[2])) == EditingResult.NOT_EXIST) {
-					sender.sendMessage("§cFake player does not exist.");
+					sender.sendMessage("Fake player not found.");
 					return;
 				}
 
 				if (output == EditingResult.OK) {
-					sender.sendMessage("§aFake player removed.");
+					sender.sendMessage("Fake player removed.");
 				}
-
 				break;
 
 			case "rename":
 				if (args.length < 4) {
-					sender.sendMessage("§cUsage: /tablist fakeplayers rename <oldName> <newName>");
+					sendUsage(sender);
 					return;
 				}
 
-				String oldName = args[2];
-				String newName = args[3];
+				String oldName = args[2], newName = args[3];
 
 				if ((output = handler.renamePlayer(oldName, newName)) == EditingResult.NOT_EXIST) {
-					sender.sendMessage("§cFake player does not exist.");
+					sender.sendMessage("Fake player not found.");
 					return;
 				}
 
 				if (output == EditingResult.OK) {
-					sender.sendMessage("§aFake player renamed.");
+					sender.sendMessage("Fake player renamed.");
 				}
-
 				break;
 
 			case "list":
-				Set<hu.montlikadani.tablist.tablist.fakeplayers.IFakePlayer> list = handler.getAllFakePlayers();
+				Set<IFakePlayer> list = handler.getAllFakePlayers();
+
 				if (list.isEmpty()) {
-					sender.sendMessage("§eNo fake players found.");
-				} else {
-					sender.sendMessage("§aFake Players:");
-					for (var fake : list) {
-						sender.sendMessage("§7- " + fake.getName());
-					}
+					sender.sendMessage("There are no fake players.");
+					return;
+				}
+
+				for (IFakePlayer fakePlayer : list) {
+					sender.sendMessage("- " + fakePlayer.getName());
 				}
 				break;
 
 			case "setskin":
-				if (args.length < 4) {
-					sender.sendMessage("§cUsage: /tablist fakeplayers setskin <name> <value> <signature>");
+				if (args.length < 5) {
+					sendUsage(sender);
 					return;
 				}
 
 				PlayerSkinProperties skinProperties = new PlayerSkinProperties(args[3], args[4]);
 
 				if ((output = handler.setSkin(args[2], skinProperties)) == EditingResult.NOT_EXIST) {
-					sender.sendMessage("§cFake player not found.");
-				} else {
-					sender.sendMessage("§aSkin applied.");
+					sender.sendMessage("Fake player not found.");
+					return;
 				}
 
+				if (output == EditingResult.OK) {
+					sender.sendMessage("Skin updated.");
+				}
 				break;
 
 			case "setping":
 				if (args.length < 4) {
-					sender.sendMessage("§cUsage: /tablist fakeplayers setping <name> <amount>");
+					sendUsage(sender);
 					return;
 				}
 
 				int amount = Util.getNumber(args[3], -1);
-
 				if ((output = handler.setPing(args[2], amount)) == EditingResult.NOT_EXIST) {
-					sender.sendMessage("§cFake player not found.");
+					sender.sendMessage("Fake player not found.");
 				} else if (output == EditingResult.PING_AMOUNT) {
-					sender.sendMessage("§cPing must be at least 1.");
+					sender.sendMessage("Invalid ping amount.");
 				} else {
-					sender.sendMessage("§aPing updated.");
+					sender.sendMessage("Ping updated.");
 				}
-
 				break;
 
 			case "setdisplayname":
 				if (args.length < 4) {
-					sender.sendMessage("§cUsage: /tablist fakeplayers setdisplayname <name> <displayName>");
+					sendUsage(sender);
 					return;
 				}
 
 				StringBuilder builder = new StringBuilder();
 				for (int i = 3; i < args.length; i++) {
-					builder.append(args[i]).append(' ');
+					builder.append(args[i]).append(" ");
 				}
 
 				output = handler.setDisplayName(args[2], builder.toString().trim().replace("\"", ""));
 
 				if (output == EditingResult.NOT_EXIST) {
-					sender.sendMessage("§cFake player not found.");
+					sender.sendMessage("Fake player not found.");
 				} else {
-					sender.sendMessage("§aDisplay name set.");
+					sender.sendMessage("Display name updated.");
 				}
-
 				break;
 
 			default:
@@ -153,12 +152,12 @@ public final class fakeplayers implements Commands.Command {
 	}
 
 	private void sendUsage(CommandSender sender) {
-		sender.sendMessage("§a/fakeplayers add <name>");
-		sender.sendMessage("§a/fakeplayers remove <name>");
-		sender.sendMessage("§a/fakeplayers rename <oldName> <newName>");
-		sender.sendMessage("§a/fakeplayers list");
-		sender.sendMessage("§a/fakeplayers setskin <name> <value> <signature>");
-		sender.sendMessage("§a/fakeplayers setping <name> <amount>");
-		sender.sendMessage("§a/fakeplayers setdisplayname <name> <displayName>");
+		sender.sendMessage("/tablist fakeplayers create <name>");
+		sender.sendMessage("/tablist fakeplayers remove <name>");
+		sender.sendMessage("/tablist fakeplayers rename <old> <new>");
+		sender.sendMessage("/tablist fakeplayers setskin <name> <value> <signature>");
+		sender.sendMessage("/tablist fakeplayers setping <name> <ping>");
+		sender.sendMessage("/tablist fakeplayers setdisplayname <name> <text>");
+		sender.sendMessage("/tablist fakeplayers list");
 	}
 }
