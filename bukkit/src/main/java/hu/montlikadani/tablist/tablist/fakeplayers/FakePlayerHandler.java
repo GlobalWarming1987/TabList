8ffec0806383d199bc6c359110a0c716a4aa34d8
GlobalWarming1987 <juggalo19076@gmail.com>
2025-07-16 21:18:53 -0400

    Update FakePlayerHandler.java

--- bukkit/src/main/java/hu/montlikadani/tablist/tablist/fakeplayers/FakePlayerHandler.java
+++ bukkit/src/main/java/hu/montlikadani/tablist/tablist/fakeplayers/FakePlayerHandler.java
@@ -1,227 +1,63 @@
-package hu.montlikadani.tablist.tablist.fakeplayers;
-
-import hu.montlikadani.tablist.utils.Util;
-import java.io.IOException;
-import java.util.Optional;
-import java.util.Set;
-
-import java.util.logging.Level;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
+package hu.montlikadani.tablist.bukkit.player;
 
 import hu.montlikadani.tablist.TabList;
-import hu.montlikadani.tablist.config.constantsLoader.ConfigValues;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class FakePlayerHandler {
 
-	private final TabList plugin;
+    private final TabList plugin;
+    private final ConcurrentHashMap<UUID, FakePlayer> fakePlayers = new ConcurrentHashMap<>();
 
-	public final Set<IFakePlayer> fakePlayers = new java.util.HashSet<>();
+    public FakePlayerHandler(TabList plugin) {
+        this.plugin = plugin;
+    }
 
-	public FakePlayerHandler(TabList plugin) {
-		this.plugin = plugin;
-	}
+    public void addPlayer(Player player) {
+        UUID uuid = player.getUniqueId();
 
-	public Optional<IFakePlayer> getFakePlayerByName(String name) {
-		if (name.isEmpty()) {
-			throw new IllegalArgumentException("The fake player name can not be empty");
-		}
+        if (fakePlayers.containsKey(uuid)) {
+            return;
+        }
 
-		for (IFakePlayer fp : fakePlayers) {
-			if (fp.getName().equalsIgnoreCase(name)) {
-				return Optional.of(fp);
-			}
-		}
+        // Run the addition of the fake player on the main server thread
+        if (!Bukkit.isPrimaryThread()) {
+            Bukkit.getScheduler().runTask(JavaPlugin.getProvidingPlugin(getClass()), () -> {
+                doAddPlayer(player);
+            });
+        } else {
+            doAddPlayer(player);
+        }
+    }
 
-		return Optional.empty();
-	}
+    private void doAddPlayer(Player player) {
+        UUID uuid = player.getUniqueId();
 
-	public void load() {
-		fakePlayers.clear();
+        if (fakePlayers.containsKey(uuid)) {
+            return;
+        }
 
-		if (!ConfigValues.isFakePlayers()) {
-			java.io.File file = plugin.getConf().getFakeplayersFile();
+        FakePlayer fake = new FakePlayer(player.getName(), uuid);
+        fake.spawn();
 
-			if (!file.exists()) {
-				return;
-			}
+        fakePlayers.put(uuid, fake);
+    }
 
-			if (file.length() == 0L) {
-				if (!file.delete()) {
-					throw new RuntimeException("Failed to delete file " + file.getName());
-				}
+    public void removePlayer(Player player) {
+        UUID uuid = player.getUniqueId();
 
-				return;
-			}
+        FakePlayer fake = fakePlayers.remove(uuid);
+        if (fake != null) {
+            fake.remove();
+        }
+    }
 
-			ConfigurationSection section = org.bukkit.configuration.file.YamlConfiguration.loadConfiguration(file)
-					.getConfigurationSection("list");
-
-			if (section != null && section.getKeys(false).isEmpty() && !file.delete()) {
-				throw new RuntimeException("Failed to delete file " + file.getName());
-			}
-
-			return;
-		}
-
-		ConfigurationSection section = plugin.getConf().getFakeplayers().getConfigurationSection("list");
-
-		if (section == null) {
-			return;
-		}
-
-		for (String name : section.getKeys(false)) {
-			FakePlayer fakePlayer = new FakePlayer(name, section.getString(name + ".displayname", ""),
-					section.getString(name + ".headuuid", ""), section.getInt(name + ".ping", -1));
-			fakePlayer.show();
-			fakePlayers.add(fakePlayer);
-		}
-	}
-
-	public void display() {
-		for (IFakePlayer fakePlayer : fakePlayers) {
-			fakePlayer.show();
-		}
-	}
-
-	public EditingResult createPlayer(String name, String displayName, final String headUUID, int ping) {
-		if (name.isEmpty()) {
-			return EditingResult.UNKNOWN;
-		}
-
-		if (getFakePlayerByName(name).isPresent()) {
-			return EditingResult.ALREADY_EXIST;
-		}
-
-		if (name.length() > 16) {
-			name = name.substring(0, 16);
-		}
-
-		if (ping < -1) {
-			ping = -1;
-		}
-
-		String path = "list." + name + ".";
-		setOptionsAndSave(path + "headuuid", headUUID, path + "ping", ping, path + "displayname", displayName);
-
-		IFakePlayer fakePlayer = new FakePlayer(name, displayName, headUUID, ping);
-		fakePlayer.show();
-		fakePlayers.add(fakePlayer);
-		return EditingResult.OK;
-	}
-
-	public void removeAllFakePlayer() {
-		fakePlayers.forEach(IFakePlayer::remove);
-		fakePlayers.clear();
-	}
-
-	public EditingResult removePlayer(String name) {
-		Optional<IFakePlayer> optional = getFakePlayerByName(name);
-
-		if (!optional.isPresent()) {
-			return EditingResult.NOT_EXIST;
-		}
-
-		ConfigurationSection section = plugin.getConf().getFakeplayers().getConfigurationSection("list");
-
-		if (section != null) {
-			for (String sName : section.getKeys(false)) {
-				if (!sName.equalsIgnoreCase(name)) {
-					continue;
-				}
-
-				section.set(sName, null);
-
-				try {
-					plugin.getConf().getFakeplayers().save(plugin.getConf().getFakeplayersFile());
-				} catch (IOException ex) {
-					Util.printTrace(Level.SEVERE, plugin, ex.getMessage(), ex);
-					return EditingResult.UNKNOWN;
-				}
-
-				break;
-			}
-		}
-
-		IFakePlayer fakePlayer = optional.get();
-
-		fakePlayer.remove();
-		fakePlayers.remove(fakePlayer);
-		return EditingResult.OK;
-	}
-
-	public EditingResult renamePlayer(final String oldName, String newName) {
-		Optional<IFakePlayer> optional = getFakePlayerByName(oldName);
-
-		if (!optional.isPresent()) {
-			return EditingResult.NOT_EXIST;
-		}
-
-		if (newName.length() > 16) {
-			newName = newName.substring(0, 16);
-		}
-
-		setOptionsAndSave("list." + newName, plugin.getConf().getFakeplayers().get("list." + oldName),
-				"list." + oldName, null);
-		optional.get().setName(newName);
-		return EditingResult.OK;
-	}
-
-	public EditingResult setSkin(String name, hu.montlikadani.tablist.utils.PlayerSkinProperties skinProperties) {
-		Optional<IFakePlayer> optional = getFakePlayerByName(name);
-
-		if (!optional.isPresent()) {
-			return EditingResult.NOT_EXIST;
-		}
-
-		setOptionsAndSave("list." + name + ".headuuid", skinProperties.playerId.toString());
-		optional.get().setSkin(skinProperties);
-		return EditingResult.OK;
-	}
-
-	public EditingResult setPing(String name, int amount) {
-		if (amount < -1) {
-			return EditingResult.PING_AMOUNT;
-		}
-
-		Optional<IFakePlayer> optional = getFakePlayerByName(name);
-
-		if (!optional.isPresent()) {
-			return EditingResult.NOT_EXIST;
-		}
-
-		setOptionsAndSave("list." + name + ".ping", amount);
-		optional.get().setPing(amount);
-		return EditingResult.OK;
-	}
-
-	public EditingResult setDisplayName(String name, String displayName) {
-		Optional<IFakePlayer> optional = getFakePlayerByName(name);
-
-		if (!optional.isPresent()) {
-			return EditingResult.NOT_EXIST;
-		}
-
-		setOptionsAndSave("list." + name + ".displayname", displayName);
-		optional.get().setDisplayName(displayName);
-		return EditingResult.OK;
-	}
-
-	private void setOptionsAndSave(Object... pathOption) {
-		FileConfiguration config = plugin.getConf().getFakeplayers();
-
-		for (int i = 0; i < pathOption.length; i += 2) {
-			config.set((String) pathOption[i], pathOption[i + 1]);
-		}
-
-		try {
-			config.save(plugin.getConf().getFakeplayersFile());
-		} catch (IOException ex) {
-			Util.printTrace(Level.SEVERE, plugin, ex.getMessage(), ex);
-		}
-	}
-
-	public enum EditingResult {
-		NOT_EXIST, ALREADY_EXIST, PING_AMOUNT, UNKNOWN, OK;
-	}
+    public void clear() {
+        fakePlayers.values().forEach(FakePlayer::remove);
+        fakePlayers.clear();
+    }
 }
