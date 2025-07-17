9476d77fafd86c250d4b94118cc197981a18fefd
GlobalWarming1987 <juggalo19076@gmail.com>
2025-07-16 23:14:59 -0400

    Update Util.java

--- bukkit/src/main/java/hu/montlikadani/tablist/utils/Util.java
+++ bukkit/src/main/java/hu/montlikadani/tablist/utils/Util.java
@@ -1,74 +1,15 @@
 package hu.montlikadani.tablist.utils;
 
-import java.util.Optional;
-import java.util.UUID;
-import java.util.logging.Level;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
-
 public final class Util {
 
-	private static final boolean MINIMESSAGE_SUPPORTED;
+	private Util() {
+	}
 
-	private static String legacyNmsVersion;
-
-	static {
-		boolean supported;
-
-		// Extra check as some library shading only classes without methods or idk
+	public static int getNumber(String s, int def) {
 		try {
-			Class.forName("net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer");
-			Class.forName("net.kyori.adventure.text.minimessage.MiniMessage").getDeclaredMethod("miniMessage");
-			supported = true;
-		} catch (ClassNotFoundException | NoSuchMethodException cn) {
-			supported = false;
+			return Integer.parseInt(s);
+		} catch (NumberFormatException ex) {
+			return def;
 		}
-
-		MINIMESSAGE_SUPPORTED = supported;
-	}
-
-	public static void consolePrint(Level level, Plugin plugin, String text, Object... params) {
-		if (hu.montlikadani.tablist.config.constantsLoader.ConfigValues.isLogConsole()) {
-			printTrace(level, plugin, text, params);
-		}
-	}
-
-	public static void printTrace(Level loggingLevel, Plugin plugin, String text, Object... parameters) {
-		plugin.getLogger().log(loggingLevel, text, parameters);
-	}
-
-	public static String legacyNmsVersion() {
-		return legacyNmsVersion == null ? legacyNmsVersion = Bukkit.getServer().getClass().getPackage().getName()
-				.split("\\.", 4)[3] : legacyNmsVersion;
-	}
-
-	public static String applyTextFormat(String value) {
-		return applyTextFormat(value, true);
-	}
-
-	@SuppressWarnings("deprecation")
-	public static String applyTextFormat(String value, boolean applyLegacyColours) {
-		if (MINIMESSAGE_SUPPORTED) {
-			value = value.replace("&", "-{-}-").replace("§", "-{-}-");
-
-			value = net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer.legacyAmpersand()
-					.serialize(net.kyori.adventure.text.minimessage.MiniMessage.miniMessage().deserialize(value))
-					.replace("-{-}-", "&");
-		}
-
-		return applyLegacyColours ? org.bukkit.ChatColor.translateAlternateColorCodes('&', value) : value;
-	}
-
-	public static Optional<UUID> tryParseId(String uuid) {
-		if (uuid == null) {
-			return Optional.empty();
-		}
-
-		try {
-			return Optional.of(UUID.fromString(uuid));
-		} catch (IllegalArgumentException ignored) {
-		}
-
-		return Optional.empty();
 	}
 }
