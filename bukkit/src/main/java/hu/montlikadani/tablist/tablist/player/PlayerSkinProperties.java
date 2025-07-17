27c82b05964cf5bed771e89bd81d6484a6197d91
GlobalWarming1987 <juggalo19076@gmail.com>
2025-07-16 23:11:00 -0400

    Update PlayerSkinProperties.java

--- bukkit/src/main/java/hu/montlikadani/tablist/tablist/player/PlayerSkinProperties.java
+++ bukkit/src/main/java/hu/montlikadani/tablist/tablist/player/PlayerSkinProperties.java
@@ -1,33 +1,12 @@
 package hu.montlikadani.tablist.tablist.player;
 
-import java.util.Objects;
-
 public final class PlayerSkinProperties {
-	private final String texture, signature;
 
-	public PlayerSkinProperties(String texture, String signature) {
-		this.texture = texture;
+	public final String value;
+	public final String signature;
+
+	public PlayerSkinProperties(String value, String signature) {
+		this.value = value;
 		this.signature = signature;
-	}
-
-	public String getTexture() {
-		return texture;
-	}
-
-	public String getSignature() {
-		return signature;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof PlayerSkinProperties)) return false;
-		PlayerSkinProperties that = (PlayerSkinProperties) o;
-		return Objects.equals(texture, that.texture) && Objects.equals(signature, that.signature);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(texture, signature);
 	}
 }
